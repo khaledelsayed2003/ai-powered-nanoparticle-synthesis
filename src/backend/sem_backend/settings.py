ee9703397a32137534d898071c2a5111e7ee9cf2
ziad <zeyad.officemail@gmail.com>
2025-12-06 16:24:28 +0300

    Django API Endpoint for prediction

--- src/backend/sem_backend/settings.py
+++ src/backend/sem_backend/settings.py
@@ -31,14 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django defaults...
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third‑party
     'rest_framework',
+
+    # Your apps
+    'prediction',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
