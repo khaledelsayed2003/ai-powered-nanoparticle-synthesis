13d497fcc97430da40b0f19014c42cb5ba22109c
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-09 23:58:03 +0300

    feat(admin): register MeanSizePrediction model

--- src/backend/prediction/admin.py
+++ src/backend/prediction/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MeanSizePrediction
 
-# Register your models here.
+
+@admin.register(MeanSizePrediction)
+class MeanSizePredictionAdmin(admin.ModelAdmin):
+    list_display = ("id", "original_filename", "predicted_mean_size_nm", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("original_filename",)
